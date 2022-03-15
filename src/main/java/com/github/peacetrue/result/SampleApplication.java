d64879585b3a68232ec58071d8d2394340756562
xiayx <xiayouxue@gold32.com>
2022-03-15 18:20:17 +0800

    基础版本

---
+++ src/main/java/com/github/peacetrue/result/SampleApplication.java
@@ -0,0 +1,14 @@
+package com.github.peacetrue.result;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author peace
+ **/
+@SpringBootApplication
+public class SampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+}
